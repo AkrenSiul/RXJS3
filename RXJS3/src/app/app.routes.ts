5b8c295d009e8fbe33e12773224345ee4203d941
AkrenSiul <167196142+AkrenSiul@users.noreply.github.com>
2025-02-08 20:43:52 +0100

    juguete services added

--- RXJS3/src/app/app.routes.ts
+++ RXJS3/src/app/app.routes.ts
@@ -1,14 +1,42 @@
 import { Routes } from '@angular/router';
 import {JugueteListComponent} from './components/juguetes/juguete-list/juguete-list.component';
+import {AddJuguetesComponent} from './components/juguetes/add-juguetes/add-juguetes.component';
+import {CosmeticoListComponent} from './components/cosmeticos/cosmetico-list/cosmetico-list.component';
+import {AddCosmeticosComponent} from './components/cosmeticos/add-cosmeticos/add-cosmeticos.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'juguete-list',
+    redirectTo: 'juguetes-list',
     pathMatch: "full"
   },
   {
-    path: 'juguete-list',
+    path: 'juguetes-list',
     component: JugueteListComponent
+  },
+  {
+    path: 'add-juguete',
+    component: AddJuguetesComponent
+  },
+  {
+    path: 'edit-juguete/:id',
+    component: AddJuguetesComponent
+  },
+  {
+    path: 'cosmeticos-list',
+    component: CosmeticoListComponent
+  },
+  {
+    path: 'add-cosmetico',
+    component: AddCosmeticosComponent
+  },
+  {
+    path: 'edit-cosmetico/:id',
+    component: AddCosmeticosComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'juguetes-list',
+    pathMatch: "full"
   }
 ];
--- RXJS3/src/app/services/juguete.service.ts
+++ RXJS3/src/app/services/juguete.service.ts
@@ -1,18 +1,32 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {JugueteInterface} from '../common/juguete-interface';
+import {Juguete, JugueteInterface} from '../common/juguete-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JugueteService {
   private readonly http: HttpClient = inject(HttpClient);
+  urlBase = 'https://api-juguetes.vercel.app/api/v2/juguete/';
 
   constructor() { }
 
-  getJuguete(page: number): Observable<JugueteInterface>{
+  getJuguetes(page: number): Observable<JugueteInterface>{
     const url = 'https://api-juguetes.vercel.app/api/v2/juguete/juguetes?page=';
     return this.http.get<JugueteInterface>(url+page);
   }
+  getJuguete(id: string): Observable<JugueteInterface>{
+    return this.http.get<JugueteInterface>(this.urlBase+'juguete/'+id);
+  }
+  addJuguete(juguete: Juguete): Observable<JugueteInterface>{
+    return this.http.post<JugueteInterface>(this.urlBase+'juguetes/', juguete);
+  }
+  putJuguete(id:string, juguete: Juguete): Observable<any>{
+    return this.http.put<any>(this.urlBase+'update/'+id, juguete);
+  }
+  deleteJuguete(id: string): Observable<any>{
+    return this.http.delete<any>(this.urlBase+id);
+  }
+
 }
