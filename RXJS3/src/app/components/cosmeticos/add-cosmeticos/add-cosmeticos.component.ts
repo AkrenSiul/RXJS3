0312ad9d5bdb9f3274b2f4c9aff32509fe5c842f
AkrenSiul <167196142+AkrenSiul@users.noreply.github.com>
2025-02-09 16:49:30 +0100

    cosmeticos-list and cosmeticos-add added from other project

--- RXJS3/src/app/components/cosmeticos/add-cosmeticos/add-cosmeticos.component.ts
+++ RXJS3/src/app/components/cosmeticos/add-cosmeticos/add-cosmeticos.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {CosmeticoService} from '../../../services/cosmetico.service';
 
 @Component({
   selector: 'app-add-cosmeticos',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './add-cosmeticos.component.html',
   styleUrl: './add-cosmeticos.component.css'
 })
 export class AddCosmeticosComponent {
+  private readonly cosmeticoService: CosmeticoService = inject(CosmeticoService);
+  private readonly formBuilder: FormBuilder = inject(FormBuilder);
+
+  formCosmetico: FormGroup = this.formBuilder.group(
+    {
+      name: [''],
+      image: [''],
+      type: [''],
+      brand: [''],
+      price: [0],
+    }
+  )
+
+  get name(): any {
+    return this.formCosmetico.get('name')
+  }
+  get image(): any {
+    return this.formCosmetico.get('image')
+  }
+  get type(): any {
+    return this.formCosmetico.get('type')
+  }
+  get brand(): any {
+    return this.formCosmetico.get('brand')
+  }
+  get price(): any {
+    return this.formCosmetico.get('price')
+  }
+
+
+  onSubmit() {
+    this.cosmeticoService.addCosmetico(this.formCosmetico.getRawValue()).subscribe(
+      {
+        next: value => {
+          console.log(value);
+        },
+        error: error => {
+          console.log(error);
+        },
+        complete: () =>{
+          console.log('Cosmetico añadido')
+        }
+      },
+    )
+  }
 
 }
--- RXJS3/src/app/components/cosmeticos/cosmetico-list/cosmetico-list.component.ts
+++ RXJS3/src/app/components/cosmeticos/cosmetico-list/cosmetico-list.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {CurrencyPipe} from '@angular/common';
+import {CosmeticoService} from '../../../services/cosmetico.service';
+import {Cosmetico} from '../../../common/cosmeticos-interface';
 
 @Component({
   selector: 'app-cosmetico-list',
-  imports: [],
+  imports: [
+    RouterLink,
+    CurrencyPipe
+  ],
   templateUrl: './cosmetico-list.component.html',
   styleUrl: './cosmetico-list.component.css'
 })
 export class CosmeticoListComponent {
+  @Input('id')id!: string;
+  private readonly cosmeticoService: CosmeticoService = inject(CosmeticoService);
+  cosmeticos: Cosmetico[] = [];
+  currentPage = 1;
+  sizePage = 20;
+
+  constructor(){
+    this.getCosmeticos();
+  }
+
+  private getCosmeticos() {
+    return this.cosmeticoService.getCosmeticos(this.currentPage,this.sizePage).subscribe(
+      {
+        next: value => {
+          this.cosmeticos = value.cosmeticos.cosmeticos;
+        },
+        error: error => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('Cosmeticos traídos');
+        }
+      }
+    );
+  }
+
+
+  onDelete(id: string) {
+    confirm("Estás seguro de que quieres eliminar este producto?")
+    {
+      return this.cosmeticoService.deleteCosmetico(this.id).subscribe(
+        {
+          next: value => {
+            console.log(value);
+          },
+          error: error => {
+            console.log(error);
+          },
+          complete: () => {
+            console.log('Elemento eliminado')
+          }
+        }
+      )
+    }
+  }
 
 }
